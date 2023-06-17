321a9d496cfa5c0990395debfbd5225e7e5706f1
Carol Mirabal <carolmirabal27@gmail.com>
2023-06-17 19:33:30 -0400

    creación de Controlador, Repo y Modelo

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,25 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\TaskRepo;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    /**
+     * taskRepo
+     *
+     * @var mixed
+     */
+    private $taskRepo;
 
+    /**
+     * __construct
+     *
+     * @param  mixed $taskRepo
+     * @return void
+     */
+    public function __construct(TaskRepo $taskRepo)
+    {
+        $this->taskRepo = $taskRepo;
+    }
+
+    /**
+     * getAll
+     *
+     * Metódo que obtiene todas las tareas
+     *
+     * @return void
+     */
     public function getAll()
     {
+        $tasks = $this->taskRepo->all();
+        $response = ['message' => "Todas las tareas", "data" => $tasks];
+        return response()->json($response, 200);
     }
 
+    /**
+     * create
+     *
+     * Metodo que guarda una tarea
+     *
+     * @param  mixed $request
+     * @return void
+     */
     public function create(Request $request)
     {
+        $description = $request->description;
+        $task = $this->taskRepo->store($description);
+        $response = ['message' => "Has creado una nueva tarea", "data" => $task];
+        return response()->json($response, 201);
     }
 
-    public function update($id)
+    /**
+     * update
+     *
+     * Metodo que actualiza una tarea
+     *
+     * @param  mixed $id
+     * @param  mixed $request
+     * @return void
+     */
+    public function update($id, Request $request)
     {
+        $description = $request->description;
+        $task = $this->taskRepo->update($id, $description);
+        $response = ['message' => "La tarea ha sido actualizada", "data" => $task];
+        return response()->json($response, 200);
     }
 
-
+    /**
+     * delete
+     *
+     * Metodo que elimina una tarea
+     *
+     * @param  mixed $id
+     * @return void
+     */
     public function delete($id)
     {
+        $task = $this->taskRepo->delete($id);
+        $response = ['message' => "La tarea ha sido eliminada", "data" => $task];
+        return response()->json($response, 200);
     }
 }
---
+++ app/Repositories/TaskRepo.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Task;
+
+class TaskRepo
+{
+    /**
+     * store
+     *
+     * @param  mixed $description
+     * @return void
+     */
+    public function store($description)
+    {
+        $task = new Task;
+        $task->description = $description;
+        $task->save();
+
+        return $task;
+    }
+
+    /**
+     * delete
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function delete($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return $task;
+    }
+
+    /**
+     * update
+     *
+     * @param  mixed $id
+     * @param  mixed $description
+     * @return void
+     */
+    public function update($id, $description)
+    {
+        $task =  Task::where('id', $id)->update(['description' => $description]);
+
+        return $task;
+    }
+
+    /**
+     * all
+     *
+     * @return void
+     */
+    public function all()
+    {
+        $task =  Task::orderBy('name')->get();
+        return $task;
+    }
+}
