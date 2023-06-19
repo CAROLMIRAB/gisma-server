ebb0df5840853ead98a27ac0877dec65d2eefb83
Carol Mirabal <carolmirabal27@gmail.com>
2023-06-19 17:40:04 -0400

    cree Replace view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -410,7 +410,7 @@
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
-                <div class="grid grid-cols-1 md:grid-cols-2">
+                <div class="grid grid-cols-1 md:grid-cols-1">
                     <div class="p-6">
                         <div class="flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -422,7 +422,7 @@
                                     class="underline text-gray-900 dark:text-white">Routes disponibles</a></div>
                         </div>
 
-                        <div class="ml-12">
+                        <div>
                             <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
                                 GET - <a href="{{ route('all-task') }}"
                                     class="text-sm text-gray-700 dark:text-gray-500 underline">{{ route('all-task') }}</a></br>
